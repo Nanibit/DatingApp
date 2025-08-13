62764d19b9d690d2b4e52b294af49627ba362100
Nanibit <nanibit256@gmail.com>
2025-08-13 12:40:14 +0200

    Seccion 3 - video 23 Making HTTP request in Angular

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'client';
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected title = 'Dating app';
+
+  ngOnInit(): void {
+    // el Get devuelve un observable (herramienta para gestionar flujos de datos asíncronos)
+    // por lo que hay que suscribirse
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: Response => console.log('next: ', Response),
+      error: error => console.error('There was an error!', error),
+      complete: () => console.log('Request completed')
+    })
+  }
 }
