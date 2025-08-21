888e16edb68f82dbaa7ec03faeca65cd5cfe6378
Nanibit <nanibit256@gmail.com>
2025-08-21 11:11:45 +0200

    Sección 4 - Video 34 Creating an Account Controller

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,4 +5,8 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    // Almacenar un hash de contraseñas
+    public required byte[] PasswordHash { get; set; }
+    // Almacenar matriz para Salt - poner el hash aleatorio
+    public required byte[] PasswordSalt { get; set; }
 }
